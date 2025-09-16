8f5898e18867661144b70319cec9e9773dbbd6e7
Jyotirmoy Chakraborty <jyotirmoy.it@gmail.com>
2025-09-16 14:07:54 -0500

    adding base code

---
+++ src/main/java/com/ice/bond/practiee/portfolio_risk_analyzer/PortfolioRiskAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.ice.bond.practiee.portfolio_risk_analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioRiskAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioRiskAnalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ice/bond/practiee/portfolio_risk_analyzer/PortfolioRiskAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ice.bond.practiee.portfolio_risk_analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioRiskAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
